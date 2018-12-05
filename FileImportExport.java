91f2a7bf74dc0f44e01f9f12685a58c6916edf76
hyro64 <cmandujano10121990@gmail.com>
2018-12-05 10:04:51 -0700

    Update FileImportExport.java
    Has default arrays for testing but is coded for export through constructor

--- FileImportExport.java
+++ FileImportExport.java
@@ -4,39 +4,53 @@ import java.io.IOException;
 
 
 public class FileImportExport {
-    static int playerSize = 6;
+    static int playerSize = 1;
+    static int playerSaveSize[] = new int[playerSize];
+    static String fileName = "Op1.txt";
+    static String playerNames[] = {"James","Jake"};
+    static String playerLvl [] = {"6","30"};
+    static String playerAc [] = {"0","50"};
+    static String playerHp [] = {"10","30"};
+    static String playerStr [] = {"0","10"};
+    static String playerDex [] = {"20","15"};
+    static String playerCon [] = {"18","9"};
+    static String playerInt [] = {"20","18"};
+    static String playerWis [] = {"15","10"};
+    static String playerCha [] = {"20","17"};
 
-    FileImportExport(int tDPlayerSize){
+    FileImportExport(int tDPlayerSize, String tD_names[], String tD_playerLvl[], String tD_PlayerAC[],
+                     String tD_PlayerHP[], String tD_PlayerStr[], String tD_PlayerDex[], String tD_PlayerCon[],
+                     String tD_PlayerInt[], String tD_PlayerWis[], String tD_PlayerCha[]){
         playerSize = tDPlayerSize;
-    }
-    public static void main(String[] args) {
-        String fileName = "Op1.txt";
-        String names[] = {"James","Jake"};
-        String playerLvl [] = new String[playerSize];
-        String playerAc [] = new String[playerSize];
-        String playerHp [] = new String[playerSize];
-        String playerStr [] = new String[playerSize];
-        String playerDex [] = new String[playerSize];
-        String playerCon [] = new String[playerSize];
-        String playerInt [] = new String[playerSize];
-        String playerWis [] = new String[playerSize];
-        String playerCha [] = new String[playerSize];
-
-        createFile(fileName,saveCampaign(1, names));
-
+        playerNames = tD_names;
+        playerLvl = tD_playerLvl;
+        playerAc = tD_PlayerAC;
+        playerHp = tD_PlayerHP;
+        playerStr = tD_PlayerStr;
+        playerDex = tD_PlayerDex;
+        playerCon = tD_PlayerCon;
+        playerInt = tD_PlayerInt;
+        playerWis = tD_PlayerWis;
+        playerCha = tD_PlayerCha;
     }
 
-    public static String saveCampaign(int playerIndex, String playerName[],String playerLvl [],String playerAc [],
-                                      String playerHp [],String playerStr [],String playerDex [],String playerCon [],
-                                      String playerInt [],String playerWis[],String playerCha []) {
+    public static String saveCampaign( int playerSaveSize[], String playerNames[], String playerLvl[], String playerAc[],
+                                      String playerHp[], String playerStr[], String playerDex[], String playerCon[],
+                                      String playerInt[], String playerWis[], String playerCha[]) {
         String content = "";
-        for (int i = 0; i< (playerIndex+1); i++) {
+        for (int i = 0; i <= playerSaveSize.length; i++) {
             content += "\n" +
                     "PlayerIndex:" + i +"\n"+
-                    "Name:"+ playerName[i] + "\n"+
-                    "Player Lvl: " + playerLvl[i] +"\n",
-                    "" +  + "\n";
-
+                    "Name:"+ playerNames[i] + "\n"+
+                    "Player Lvl: " + playerLvl[i] +"\n" +
+                    "Amour Class:" + playerAc[i] + "\n" +
+                    "Hit:points" + playerHp[i] + "\n" +
+                    "Strength:" + playerStr[i] + "\n" +
+                    "Dexterity:" + playerDex[i] + "\n" +
+                    "Constitution:" + playerCon[i] + "\n" +
+                    "Intelligence:" + playerInt[i] + "\n" +
+                    "Wisdom:" + playerWis[i] + "\n" +
+                    "Charisma:" + playerCha[i] + "\n";
         }
         return content;
     }
@@ -61,4 +75,12 @@ public class FileImportExport {
             }
         }
     }
+
+    public static void main(String[] args) {
+        createFile(fileName, saveCampaign(playerSaveSize, playerNames, playerLvl, playerAc,
+        playerHp, playerStr, playerDex, playerCon, playerInt, playerWis, playerCha));
+
+    }
+
+
 }
